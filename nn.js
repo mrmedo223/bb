05db3b7289073b79b09bfef7c9873eba6ae4152d
mrmedo223 <42041569+mrmedo223@users.noreply.github.com>
2018-08-02 20:18:39 +0200

    nn

---
+++ nn.js
@@ -0,0 +1,12 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document)['ready'](function () {
+        $('#cnmuprot')['html']('<a href="http://www.mr-medo.com" rel="dofollow" target="_blank">مستر ميدو</a>');
+        setInterval(function () {
+                if (!$('#cnmuprot:visible')['length']) {
+                  window['location']['href'] = 'http://www.mr-medo.com';
+                };
+            }, 3000);
+    });
+//]]>
+</script>
